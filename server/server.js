f3c28995fe35186042b98dbe6440c0b7f2ed7e38
HargunS7 <Hargun3574@gmail.com>
2024-11-08 21:55:58 +0530

    Tokens

--- server/server.js
+++ server/server.js
@@ -16,34 +16,34 @@ app.use(express.static("public"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const users = [
-    { name: "Hargun Singh", age: 20 },
-    { name: "Hindveer", age: 19 },
-    { name: "Jaikirat", age: 20 },
+{ name: "Harman Dhiman", age: 20 },
+{ name: "Hindveer", age: 19 },
+{ name: "Jaikirat", age: 20 },
 ];
 
 const port = 3000 || 5000;
 const dotenv = require("dotenv");
- dotenv.config();
- connectDb();
+dotenv.config();
+connectDb();
 app.use(express.json());
 app.use(cors());
 app.get('/' , (req , res)=>{
-    res.send("working");
+res.send("working");
 })
 app.set('view engine' , 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 app.get("/home",(req , res)=>{
-    res.render("home" , {
-       username:" Hargun",
-       posts : " time pass"
-    })
+res.render("home" , {
+username:" Harman Dhiman",
+posts : " time pass"
+})
 })
 app.get("/alluser", (req, res) => {
-    res.render("alluser", {
-        users: users, 
-    });
+res.render("alluser", {
+users: users, 
+});
 });
 // const storage = multer.diskStorage({
 //     destination: (req, file, cb) => {
@@ -57,46 +57,48 @@ app.get("/alluser", (req, res) => {
 
 // Set up GridFsStorage for file storage in MongoDB
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-  })
-  
+destination: function (req, file, cb) {
+cb(null, './uploads')
+},
+filename: function (req, file, cb) {
+const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+cb(null, file.fieldname + '-' + uniqueSuffix)
+}
+})
+
 //   const upload = multer({ storage: storage })
- const upload = multer({storage : storage});
+const upload = multer({storage : storage});
 app.post("/profile", upload.single("avatar"), async function(req, res , next) {
     
-    console.log(req.body);
-    console.log(req.file);
-    // console.log(req.file.path)
-    let {title} = req.body;
-    let {path} = req.file;
+console.log(req.body);
+console.log(req.file);
+// console.log(req.file.path)
+let {title} = req.body;
+let {path} = req.file;
     // let newProfie= new Profie({image:req.file.path});
     // await newProfie.save()
     // res.render("profile",{image:req.file.path})
     // const imagePath = "/uploads/" + req.file.filename.replace(/\\/g, "/");
-    let newProfie = new Profie({ title:title ,  image: path });
-    await newProfie.save();
-    res.render("profile", { image: path });
+let newProfie = new Profie({ title:title ,  image: path });
+await newProfie.save();
+res.render("profile", { image: path });
     // return res.redirect("/home");
+    
 });
 app.get("/profile",async(req,res)=>{
-    
-    let allblog=await Profie.find();
+
+let allblog=await Profie.find();
     // console.log(allblog)
     // console.warn("THISHSHSHSH")
-    console.log("working");
-    
-    res.render("profile",{profile : allblog});
+    console.log("chalgya");
+    console.log("chalgya oyee");
+
+res.render("profile",{profile : allblog});
 }) 
 // app.get("/profile", async (req, res) => {
 //     let profile = await Profie.findOne();
 //     console.log(profile)
-    
+
 //     if (profile) {
 //         // console.log("Image URL:"+ profile.image);  // Log the image path
 //         res.render("profile", { image: profile.image });
@@ -110,5 +112,4 @@ app.get("/profile",async(req,res)=>{
 app.use("/api/register" , require("./routes/userRoutes"));
 app.use("/api/doctors", doctorsDetails);
 app.listen(port , ()=>{
-    console.log(`server running on http://localhost:${port}`);
-})
+console.log(`server running on http://localhost:${port}`);})
